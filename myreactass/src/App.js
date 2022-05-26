ac32ac9cd978077306d7365848f5e7e7d5cab180
Ajayraoyadav <97391719+Ajayraoyadav@users.noreply.github.com>
2022-05-26 08:12:19 +0530

    update todo

--- myreactass/src/App.js
+++ myreactass/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Apptic from './RCT-101.P4 - React - State Management and Uplifting/TIC TAC TOE/Apptic';
+import {TodoList} from './RCT-101.P4 - React - State Management and Uplifting/REACT TODOS/TodoList';
 
 function App() {
   return (
     <div className="App">
-     <Apptic/>
-     
+     {/* <Apptic/> */}
+        <TodoList/>
     </div>
   );
 }
