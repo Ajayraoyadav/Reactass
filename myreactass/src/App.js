11a12035f85e653896a0672fd9ef40dac0b66ba4
Ajayraoyadav <97391719+Ajayraoyadav@users.noreply.github.com>
2022-05-26 08:28:52 +0530

    update timer

--- myreactass/src/App.js
+++ myreactass/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Apptic from './RCT-101.P4 - React - State Management and Uplifting/TIC TAC TOE/Apptic';
 import {TodoList} from './RCT-101.P4 - React - State Management and Uplifting/REACT TODOS/TodoList';
 import { Todos } from './RCT-101.W2.A1 - React - useEffect 1/REACT - USEEFFECT 1/Todos';
+import Timer from './RCT-101.W2.A2 - React - useEffect 2/TIMER/Timer';
 
 function App() {
   return (
     <div className="App">
      {/* <Apptic/> */}
      {/* <TodoList/> */}
-     <Todos/>
+     {/* <Todos/> */}
+     <Timer/>
 
     </div>
   );
