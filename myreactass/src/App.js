623c18dff26f4dfb2a4b138fc48d68ad36e2d0a7
Ajayraoyadav <97391719+Ajayraoyadav@users.noreply.github.com>
2022-05-30 16:51:08 +0530

    update

--- myreactass/src/App.js
+++ myreactass/src/App.js
@@ -1,25 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Apptic from "./RCT-101.P4 - React - State Management and Uplifting/TIC TAC TOE/Apptic";
-import { TodoList } from "./RCT-101.P4 - React - State Management and Uplifting/REACT TODOS/TodoList";
-import { Todos } from "./RCT-101.W2.A1 - React - useEffect 1/REACT - USEEFFECT 1/Todos";
-import Timer from "./RCT-101.W2.A2 - React - useEffect 2/TIMER/Timer";
-import GoogleTimer from "./RCT-101.W2.A3 - Events, useRef, Forms/Google Timer/GoogleTimer";
-import Form from "./RCT-101.W2.A3 - Events, useRef, Forms/React Forms/Form";
-import CreditCardForm from "./RCT-101.W2.A4 - Chakra-UI /CHAKRA-UI CREDIT CARD/components/CreditCardForm";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      {/* <Apptic/> */}
-      {/* <TodoList/> */}
-      {/* <Todos/> */}
-      {/* <Timer/> */}
-      {/* <GoogleTimer/> */}
-      {/* <Form/> */}
-      <CreditCardForm/>
-    </div>
-  );
-}
+const App = () => {
+  return <div>{/* TODO: Code here */}</div>;
+};
 
 export default App;
--- myreactass/src/RCT-101.W2.A3 - Events, useRef, Forms/React Forms/FormFetch.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function FormFetch() {
-  return (
-    <>
-    <h1>Form Details</h1>
-    </>
-  )
-}
