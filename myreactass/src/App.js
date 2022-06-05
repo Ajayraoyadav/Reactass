2531f473302286dcdbdd91f6f698f57fe90116a2
Ajayraoyadav <97391719+Ajayraoyadav@users.noreply.github.com>
2022-06-05 09:08:17 +0530

    update

--- myreactass/src/App.js
+++ myreactass/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css"
 import Employee from "./EmployeeForm/Employee";
+import AllRoutes from "./RCT-101.W3.A1 - Context API/AUTH CONTEXT/Pages/AllRoutes";
 import Navbar from "./RCT-101.W3.A1 - Context API/components/Navbar";
 
 const App = () => {
   return(
     <>
-    <Employee/>
+    {/* <Employee/> */}
+    <AllRoutes/>
     </>
   )
 };
